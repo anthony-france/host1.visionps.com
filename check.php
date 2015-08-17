0b54c7940b21cf4ca20076a27d27d6a81a763151
Anthony France <anthony-france@users.noreply.github.com>
2015-08-16 19:05:26 -0500

    Update check.php

--- check.php
+++ check.php
@@ -2,5 +2,6 @@
 header('Content-Type: text/plain');
 echo "Server IP: ".$_SERVER['SERVER_ADDR'];
 echo "\nClient IP: ".$_SERVER['REMOTE_ADDR'];
+echo "\nDocument Root: ".$_SERVER['DOCUMENT_ROOT'];
 echo "\nX-Forwarded-for: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
 ?>
