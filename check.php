d3191aa6cfb6e311b2ab84d22d1350e0dc8c9366
Anthony France <afrance@visionps.com>
2015-08-22 14:37:20 -0500

    check sql is accessable

--- check.php
+++ check.php
@@ -5,3 +5,14 @@ echo "\nClient IP: ".$_SERVER['REMOTE_ADDR'];
 echo "\nDocument Root: ".$_SERVER['DOCUMENT_ROOT'];
 echo "\nX-Forwarded-for: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
 ?>
+
+<?php
+$username = "haproxy_check";
+$password = "check";
+$hostname = "ipr-lbapp-chi51.visionps.com"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+  or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
+?>
