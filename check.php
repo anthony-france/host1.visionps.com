9a6f42739452acea2beb0efbfdb40180b3bea93f
Anthony France <afrance@visionps.com>
2015-08-22 15:13:57 -0500

    more host info

--- check.php
+++ check.php
@@ -1,18 +1,30 @@
+
+<h1>Server Info</h1>
+<pre>
 <?php
-header('Content-Type: text/plain');
 echo "Server IP: ".$_SERVER['SERVER_ADDR'];
 echo "\nClient IP: ".$_SERVER['REMOTE_ADDR'];
 echo "\nDocument Root: ".$_SERVER['DOCUMENT_ROOT'];
 echo "\nX-Forwarded-for: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
 ?>
+</pre>
 
+<h1>mysql check</h1>
 <?php
 $username = "check";
 $password = "check";
 $hostname = "ipr-lbapp-chi51.visionps.com"; 
 
+<pre>
+<?php
+	echo "Username: $username";
+	echo "Password: $password";
+	echo "Hostname: $hostname";
+?>
+
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) 
-  or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>";
+   or die("Unable to connect to $hostname");
+  
+echo "Connected to $hostname<br>";
 ?>
