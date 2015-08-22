8ca20934271b09a91b988830afe33d8e2093f424
Anthony France <afrance@visionps.com>
2015-08-22 15:27:32 -0500

    check updates

--- check.php
+++ check.php
@@ -30,4 +30,13 @@ $dbhandle = mysql_connect($hostname, $username, $password)
 echo "Connected to $hostname<br>";
 ?>
 
+<?php
+//execute the SQL query and return records
+$result = mysql_query("SELECT @@hostname");
+//fetch tha data from the database
+while ($row = mysql_fetch_array($result)) {
+   echo "sql server hostname:".$row{'hostname'};
+}
+?>
+
 </pre>
