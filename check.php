0dc4dc86cbfb7d4a04bc134ebc3bd7a88b2090a4
Anthony France <afrance@visionps.com>
2015-08-22 15:29:22 -0500

    debug help

--- check.php
+++ check.php
@@ -33,6 +33,7 @@ echo "Connected to $hostname<br>";
 <?php
 //execute the SQL query and return records
 $result = mysql_query("SELECT @@hostname");
+var_dump($result);
 //fetch tha data from the database
 while ($row = mysql_fetch_array($result)) {
    echo "sql server hostname:".$row{'hostname'};
