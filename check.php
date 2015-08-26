0f2c291c61c24e21f4035d1c7aaa1051b212b917
Anthony France <afrance@visionps.com>
2015-08-25 23:20:23 -0500

    dd

--- check.php
+++ check.php
@@ -44,7 +44,7 @@ while ($row = mysql_fetch_array($result)) {
 <h1>mysql check 02</h1>
 <pre>
 <?php
-    $username = "haproxy_check";
+    $username = "check";
     $password = "check";
     $hostname = "ipr-lbapp-chi52.visionps.com"; 
 	
